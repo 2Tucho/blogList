16624126d7ed678464c5b1284de5f0fa894835dd
2Tucho <albergromero@gmail.com>
2025-06-10 14:15:18 +0200

    Added exercise 4.2

--- app.js
+++ app.js
@@ -2,20 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const listRouter = require("./controllers/blogs")
 const middleware = require('./utils/middleware')
 
 const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose
     .connect(config.MONGODB_URI)
@@ -29,19 +21,7 @@ mongoose
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use("/api/blogs", listRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ controllers/blogs.js
@@ -0,0 +1,21 @@
+const listsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+listsRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+listsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result)
+    })
+    .catch((error) => next(error))
+})
+
+module.exports = listsRouter
