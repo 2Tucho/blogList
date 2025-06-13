c06093fefc9c18950e16aa4598ec9b5bbcbcdb1c
2Tucho <albergromero@gmail.com>
2025-06-13 12:28:12 +0200

    Added exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
-  return 1
+    return 1
+}
+
+const totalLikes = (blog) => {
+    let count = 0
+    for (let i = 0; i < blog.length; i++) {
+        count += blog[i].likes
+    }
+    return count
 }
 
 module.exports = {
-  dummy
+    dummy,
+    totalLikes
 }
