250291e435119d72bcfb83228a03c9810aef063e
2Tucho <albergromero@gmail.com>
2025-06-13 13:14:02 +0200

    Added exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,15 +2,27 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (blog) => {
+const totalLikes = (blogs) => {
     let count = 0
-    for (let i = 0; i < blog.length; i++) {
-        count += blog[i].likes
+    for (let i = 0; i < blogs.length; i++) {
+        count += blogs[i].likes
     }
     return count
 }
 
+const favoriteBlog = (blogs) => {
+    let likesArr = []
+    for (let i = 0; i < blogs.length; i++) {
+        likesArr.push(blogs[i].likes)
+    }
+    let maxLikes = Math.max(...likesArr)
+    let finalBlog = blogs.filter(elem => elem.likes === maxLikes)[0]
+    const { _id, url, __v, ...res } = finalBlog
+    return res
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
