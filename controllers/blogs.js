29d98e70d420da879ef38015193085b66ed3d391
2Tucho <albergromero@gmail.com>
2025-07-14 13:02:18 +0200

    Added exercise 4.15 part 1

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,51 +1,60 @@
 const listsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-listsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
+listsRouter.get('/', async (request, response) => {
+    const blogs = await Blog
+        .find({}).populate('user', { username: 1, name: 1 })
+
     response.json(blogs)
-  })
+
 })
 
 listsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
-  if (blog) {
-    response.json(blog)
-  } else {
-    response.status(404).end()
-  }
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
 })
 
-listsRouter.post('/', async (request, response, next) => {
-  const body = request.body
+listsRouter.post('/', async (request, response) => {
+    const body = request.body
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
+    const user = await User.findById(body.userId)
 
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+    const blog = new Blog({
+        title: body.title,
+        author: user.name,
+        url: body.url,
+        likes: body.likes,
+        user: user.id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
 })
 
 listsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
 })
 
 listsRouter.put('/:id', async (request, response) => {
-  const updatedBlog = await Blog.findByIdAndUpdate(
-    request.params.id,
-    request.body, //{ request.body.likes } si solamente quisiese actualizar los likes
-    { new: true, runValidators: true }
-  )
-  if (updatedBlog) {
-    response.json(updatedBlog)
-  } else {
-    response.status(404).end()
-  }
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        request.params.id,
+        request.body, //{ request.body.likes } si solamente quisiese actualizar los likes
+        { new: true, runValidators: true }
+    )
+    if (updatedBlog) {
+        response.json(updatedBlog)
+    } else {
+        response.status(404).end()
+    }
 })
 
 module.exports = listsRouter
